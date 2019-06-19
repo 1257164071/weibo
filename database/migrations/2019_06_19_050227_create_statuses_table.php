a43213263abeb79175f5c88384c6434d88569385
NightDays <1257164071@qq.com>
2019-06-19 14:33:56 +0800

    微博数据模型

---
+++ database/migrations/2019_06_19_050227_create_statuses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id')->comment('用户发布信息表');
+            $table->text('content')->comment('存微博的内容');
+            $table->integer('user_id')->index()->comment('微博发布者的个人 id');
+            $table->index(['created_at']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
